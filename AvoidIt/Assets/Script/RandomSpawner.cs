3f963189f34da5252dc88e12b843454697c5cc25
unknown <laner1371@gmail.com>
2025-06-06 17:17:39 +0900

    [feat] #3 - 투사체 발사

--- AvoidIt/Assets/Script/RandomSpawner.cs
+++ AvoidIt/Assets/Script/RandomSpawner.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class RandomSpawner : MonoBehaviour
 {
-    public GameObject prefabToSpawn;    //투사체 원본
+    public GameObject obj;
+    public GameObject dynamite; //다이너마이트 원본
+    public GameObject bullet;    //총알 원본
     public GameObject Player;   //플레이어
     public int playerCollisionCount = 0;    //충돌 횟수
     public float minDistanceFromPlayer = 0.5f;    //플레이어와 투사체의 최소간격
@@ -21,7 +23,7 @@ public class RandomSpawner : MonoBehaviour
 
     void SpawnAndLaunchObject()
     {
-        if (spawnCount >= maxSpawnCount) return;    //종료조건(재설정 필요요)
+        if (spawnCount >= maxSpawnCount) return;    //종료조건(재설정 필요)
 
         float clonesToSpawn = spawnCount / 10 + 1;    //한번에 소환되는 투사체의 갯수
         float min = minLaunchForce*(clonesToSpawn/2);
@@ -46,18 +48,31 @@ public class RandomSpawner : MonoBehaviour
                 randomPosition = new Vector3(
                 Random.Range(-9, 24),
                 5f,
-                Random.Range(7, -30)
+                Random.Range(-30, 7)
             );
                 attempt++;
             }
-    //플레이어와 너무 가까운 위치에서 투사체가 등장하지 않도록 함함
+    //플레이어와 너무 가까운 위치에서 투사체가 등장하지 않도록 함
     while (Vector3.Distance(Player.transform.position, randomPosition) < minDistanceFromPlayer && attempt < maxAttempts);
+            float speed = Random.Range(min, max);
+            if (speed < 10f)
+            {
+                obj = Instantiate(dynamite, randomPosition, Quaternion.identity);   // 투사체의 클론을 생성
+                obj.transform.localScale = new Vector3(2f, 2f, 2f);    // 투사체 크기 확대
+            }
 
-                GameObject obj = Instantiate(prefabToSpawn, randomPosition, Quaternion.identity);   // 투사체의 클론을 생성
+            else
+            {
+                obj = Instantiate(bullet, randomPosition, Quaternion.identity);   // 투사체의 클론을 생성
                 obj.transform.localScale = new Vector3(3f, 3f, 3f);    // 투사체 크기 확대
-                Rigidbody rb = obj.GetComponent<Rigidbody>();   // 투사체에 rigidBody 속성 부여
-                obj.AddComponent<AutoDestroyWhenFall>();    // 투사체가 일정 구간을 지나면 사라지도록 함
-                obj.AddComponent<CollisionTrigger>();   // 플레이어와 투사체가 닿으면 카운트가 증가
+            }
+                
+                
+
+            
+            Rigidbody rb = obj.GetComponent<Rigidbody>();   // 투사체에 rigidBody 속성 부여
+            obj.AddComponent<AutoDestroyWhenFall>();    // 투사체가 일정 구간을 지나면 사라지도록 함
+            obj.AddComponent<CollisionTrigger>();   // 플레이어와 투사체가 닿으면 카운트가 증가
 
             if (!obj.TryGetComponent<Collider>(out Collider col))   // 투사체에 collider가 설정되어 있지 않은경우 자동설정
             {
@@ -73,12 +88,13 @@ public class RandomSpawner : MonoBehaviour
             }
             rb.useGravity = false;
             rb.linearDamping = 0f;
-            // 투사체가 xz축의의 무작위 방향으로 날아가도록 함
+            // 투사체가 xz축의 무작위 방향으로 날아가도록 함
             Vector2 random2D = Random.insideUnitCircle.normalized;
             Vector3 direction = new Vector3(random2D.x, 0f, random2D.y);
-            rb.AddForce(direction * Random.Range(min,max), ForceMode.Impulse);
+            
+            rb.AddForce(direction * speed, ForceMode.Impulse);
 
-            // 투사체의 방향을을 날아가는 방향으로 설정
+            // 투사체의 방향을 날아가는 방향으로 설정
             if (direction != Vector3.zero)
             {
                 Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.up);
